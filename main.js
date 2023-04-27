b98518e311a409bc9818961c2c1c385f0b892da2
Manuell00 <manuell.caselli@gmail.com>
2023-04-27 15:15:02 +0200

    Inserimento js

--- main.js
+++ main.js
@@ -3,3 +3,25 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va messo con massimo due decimali.
+
+let passeggeroEta, passeggeroKm, prezzoBiglietto;
+
+passeggeroEta = parseInt(prompt("Quanti anni ha l'utente?"));
+
+passeggeroKm = parseInt(prompt("Quanti km deve percorrere l'utente ")); 
+
+prezzoBiglietto = parseFloat(0.21 * passeggeroKm)
+
+if (passeggeroEta < 18){
+    prezzoBiglietto = prezzoBiglietto - (0.2 * prezzoBiglietto)
+}
+
+else if (passeggeroEta > 65){
+    prezzoBiglietto = prezzoBiglietto - (0.4 * prezzoBiglietto)
+}
+
+else{
+    prezzoBiglietto = prezzoBiglietto
+}
+
+document.getElementById("price").innerHTML = `${prezzoBiglietto.toFixed(2)} € `
